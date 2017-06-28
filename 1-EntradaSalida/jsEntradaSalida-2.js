1c99a6a53ba2688dae8aa82d7e0ecf7478fb1507
mosqueira <davidmosqueira93@gmail.com>
2017-06-28 20:32:10 -0300

    primer commit
    subimos los dos primeros ejercicios

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,10 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	var nombre = prompt("Porfavor ingresa tu nombre", "natalia natalia");
+alert ("su nombre es: " + nombre);
+//sin comillas da el valor de la variable nombrada
+// el signo de mas sirve para concatenar una frase y una variable
+
 }
 

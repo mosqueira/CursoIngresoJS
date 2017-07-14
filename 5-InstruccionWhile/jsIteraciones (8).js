8aafff6b528debdefec21b6ff92ec4ed76696d92
mosqueira <davidmosqueira93@gmail.com>
2017-07-14 20:50:43 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,8 +4,24 @@ function Mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
+	var numero;
 	var respuesta='si';
+
+	while(respuesta=='si')
+	{
+		numero=prompt("ingresar numero");
+		numero=parseInt(numero);
+		if(numero>0)
+		{
+			positivo=numero+positivo;
+		}
+		else
+		{
+			negativo=numero*negativo;
+		}
+		respuesta=prompt("Ponerle si para continuar");
+
+	}
 
 
 document.getElementById('suma').value=positivo;
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -3,15 +3,57 @@ function Mostrar()
 
 	var contador=0;
 	// declarar variables
-	
 	var respuesta='si';
+	var nota;
+	var notamayor;
+	var notamenor;
 
-	while(respuesta!='no')
+	while(contador<3)
 	{
-		
+		nota=prompt("ingresar nota");
+		nota=parseInt(nota);
+		console.log("vuelta"+contador);
+		if(notamayor==null && notamenor==null)
+		{
+			notamayor=nota;
+			notamenor=nota;
+		}
 	
-	}
+		
+		if(nota>notamayor)
+		{
+			notamayor=nota;
 
+		}
+
+	  
+			
+			if(nota<notamayor && nota<notamenor)
+			{
+				notamenor=nota;
+
+			}
+		
+			else
+			{
+				if(nota==notamayor || nota==notamenor)
+				{
+					notamayor=notamayor;
+					notamenor=notamenor;
+
+				}
+
+			}
+		contador+=1;
+
+	}	
+		
+		
+		
+
+		
+		document.getElementById('maximo').value=notamayor;
+		document.getElementById('minimo').value=notamenor;
 
 
 
